8ca184f8e38016857a05f4ff333715bb849d3a3b
Catur Hidayat <caturhidayat00@gmail.com>
2023-06-01 18:01:59 +0700

    feat: added analitycs vercel to this project

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/theme";
+import { Analytics } from '@vercel/analytics/react'
 
 
 export default function App({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
             <Component {...pageProps} />
+            <Analytics />
         </ChakraProvider>
     );
 }
