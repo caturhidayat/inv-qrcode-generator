e81ac3bb6b9253f48654f4f26b0fe9fbd66daa3f
Catur Hidayat <caturhidayat00@gmail.com>
2023-06-10 23:20:46 +0700

    feat: prettier

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,37 +3,36 @@ import React from "react";
 import Link from "next/link";
 
 export default function Header() {
-    return (
-        <Box bg='gray.100'>
-            <Box
-                display='flex'
-                py='5px'
-                alignContent='center'
-                justifyContent='center'
-            >
-                <Text
-                    bgGradient='linear(to-l, #7928CA, #FF0080)'
-                    bgClip='text'
-                    fontSize='4xl'
-                    fontWeight='extrabold'
-                >
-                    INVOICE QR-CODE GENERATOR
-                </Text>
-                <br />
-                {/* <Heading textColor=''>INVC QR-CODE</Heading> */}
-            </Box>
-            <Box display='Flex' justifyContent='center'>
-                <Button>
-                    <Link href='/' >Home</Link>    
-                </Button>
-                {/* <Button>
+  return (
+    <Box bg="blue.200">
+      <Box
+        display="flex"
+        py="5px"
+        alignContent="center"
+        justifyContent="center"
+      >
+        <Text
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+          fontSize="4xl"
+          fontWeight="extrabold"
+        >
+          INVOICE QR-CODE GENERATOR
+        </Text>
+        <br />
+        {/* <Heading textColor=''>INVC QR-CODE</Heading> */}
+      </Box>
+      <Box display="Flex" justifyContent="center">
+        <Button>
+          <Link href="/">Home</Link>
+        </Button>
+        {/* <Button>
                     <Link href='/pdf'>PDF</Link>
                 </Button> */}
-                <Button>
-                    <Link href='/decrypt'>Decrypt</Link>
-                </Button>
-
-            </Box>
-        </Box>
-    );
+        <Button>
+          <Link href="/decrypt">Decrypt</Link>
+        </Button>
+      </Box>
+    </Box>
+  );
 }
