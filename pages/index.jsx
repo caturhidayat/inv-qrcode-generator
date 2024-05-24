a7e5002db66059382212c311f04578090eaa2c22
Catur Hidayat <caturhidayat00@gmail.com>
2024-05-24 10:43:28 +0700

    docs(app): add title on page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
 import FormInput from "@/components/FormInput";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home</title>
+      </Head>
       <FormInput />
     </>
   );
